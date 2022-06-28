7d3c7760dfed738edb9ad8e12b2fd1c27f56b407
Wils Iglesias <wiglesias83@gmail.com>
2022-06-28 23:21:12 +0200

    Separate promise weather

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,49 +1,11 @@
 <script>
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '848f873f97msh9cfe21671b3d68fp161d03jsn87afb4cd275d',
-      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-    }
-  };
-
-  const weather = fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=Tortosa', options)
-    .then(response => response.json())
-    .then((response) => {
-      console.log(response);
-      const {location, current} = response;
-      const {country, localtime, name} = location;
-      const {
-        condition,
-        humidity,
-        feelslike_c,
-        is_day,
-        temp_c,
-        wind_kph, wind_dir
-      } = current;
-      const {code, text} = condition;
-
-      return {
-        conditionCode: code,
-        conditionText: text,
-        country,
-        localtime,
-        name,
-        humidity,
-        isDay: is_day,
-        feelsLike: feelslike_c,
-        temperature: temp_c,
-        windSpeed: wind_kph,
-        windDir: wind_dir
-      }
-    })
+  import { getWeatherFrom } from '../services/weather.js';
+  const weatherPromise = getWeatherFrom();
 </script>
 
-{#await weather then}
-  {console.log(weather)}
+{#await weatherPromise then weather}
+  <h1>{weather.conditionText}</h1>
 {/await}
-
-<h1>Welcome to SvelteKit</h1>
 
 <style>
   h1 {
