d8fc984052bc8931f5a08c7e623c4c15a1ab2844
Wils Iglesias <wiglesias83@gmail.com>
2022-06-29 18:33:48 +0200

    Create weather footer component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,7 +9,16 @@
     display: grid;
     place-content: center;
     min-height: 100vh;
+    margin: 0;
   }
+  :global(*, *::after, *::before) {
+    box-sizing: border-box;
+  }
+
+  :global(h1, h2, h3, h4, h5) {
+    margin: 0;
+  }
+
   div {
     aspect-ratio: 9 / 16;
     width: 400px;
@@ -17,5 +26,6 @@
     display: block;
     box-shadow: 0 0 100px rgba(0, 0, 0, 0.1);
     border-radius: 8px;
+    position: relative;
   }
 </style>
